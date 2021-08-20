139ec6aefb56c7b02672e9babd6f8cd5b1d6ac4c
Lam Nguyen <lnguyendev@gmail.com>
2021-08-19 22:15:46 -0500

    update client url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     return axios.create({
       // baseURL:
       //   'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: 'http://tix-reserve.xyz/',
+      baseURL: 'http://www.tix-reserve.xyz/',
       headers: req.headers,
     });
   } else {
